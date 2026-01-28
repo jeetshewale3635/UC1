b15caca10bbd97d74694d6dcff26fd73142b6c5d
Ajit Shewale <SH20648929@wipro.com>
2026-01-28 17:28:06 +0530

    Equals method Assignment

---
+++ FeetMeasurementEquality/src/main/java/org/example/QuantityMeasurementApp.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.util.Objects;
+
+public class QuantityMeasurementApp {
+
+    public static class Feet {
+        private final double value;
+
+
+        public Feet(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+
+            Feet feetObj = (Feet) obj;
+
+            int comValue = Double.compare(value, feetObj.value);
+
+            return comValue == 0;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(value);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Feet f1 = new Feet(1.1);
+        Feet f2 = new Feet(1.1);
+
+        System.out.println(f1.equals(f2));
+
+    }
+}
